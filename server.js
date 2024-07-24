4335dcd54354b5ddd8a6487576b5a4c03a382984
Vilius Dzemyda <dev@viliusdzemyda.com>
2024-07-24 11:32:05 +0100

    add basic server

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/api/spin', (req, res) => {
-  res.json({ POSITION: Math.floor(Math.random() * 4) + 1 });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });

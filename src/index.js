bc4a34b11442d562cf9601f6a3dd8eedf1b59a78
Vilius Dzemyda <dev@viliusdzemyda.com>
2024-07-24 15:17:04 +0100

    add basic wheel spin logic

--- src/index.js
+++ src/index.js
@@ -3,25 +3,64 @@ import './styles/main.scss';
 
 (async () => {
   const wheelContainer = document.getElementById('game-wheel');
+  const spinButton = document.getElementById('game-button');
 
+  // Create a PixiJS application
   const app = new Application();
 
+  // Initialise the application
   await app.init({
     background: '#140D3D',
     resizeTo: window,
   });
 
-  wheelContainer.innerHTML = '';
-
+  // Add the application's canvas to the wheelContainer
   wheelContainer.appendChild(app.canvas);
 
+  // Load the wheel image
   const texture = await Assets.load(require('./assets/img/wheel.png'));
 
-  const background = new Sprite(texture);
+  // Create a new Sprite from an image path
+  const wheel = new Sprite(texture);
 
-  app.stage.addChild(background);
+  // Add to stage
+  app.stage.addChild(wheel);
 
-  background.anchor.set(0.5);
-  background.x = app.screen.width / 2;
-  background.y = app.screen.height / 2;
+  // Center the sprite's anchor point
+  wheel.anchor.set(0.5);
+
+  // Move the sprite to the center of the screen
+  wheel.x = app.screen.width / 2;
+  wheel.y = app.screen.height / 2;
+
+  // Flag to control the rotation.
+  let spinning = false;
+
+  // Variable to track the rotation speed
+  const rotationSpeed = 0.1;
+
+  // Duration to spin the wheel
+  const spinDuration = 2000; // 2 seconds
+
+  // Add a click event listener to the spin button
+  spinButton.addEventListener('click', () => {
+    if (!spinning) {
+      // Start spinning if not already spinning
+      spinning = true;
+      spinButton.disabled = true; // Disable the button while spinning
+
+      // Spin for a set duration
+      setTimeout(() => {
+        spinning = false;
+        spinButton.disabled = false; // Re-enable the button after spinning
+      }, spinDuration);
+    }
+  });
+
+  // Update the wheel rotation based on the flag
+  app.ticker.add((time) => {
+    if (spinning) {
+      wheel.rotation += rotationSpeed * time.deltaTime;
+    }
+  });
 })();
